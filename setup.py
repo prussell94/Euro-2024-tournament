186e8964384e878a10793df4b76495233e9453f3
Patrick Russell <prussell94@gmail.com>
2024-09-08 14:41:38 -0400

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='euro_2024_tournament',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'torch',
         'pandas',
         # List other dependencies here
     ],
-    author='Your Name',
-    description='Euro 2024 Tournament Tournament',
+    author='Patrick Russell',
+    description='Euro 2024 Tournament',
     url='https://github.com/prussell94/Euro-2024-tournament',
 )
